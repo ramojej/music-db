9b1b48c2073ff497ff3bf0aa4e509e256cb571db
Jejomar <jejomardorongon@gmail.com>
2017-08-15 17:56:14 +0800

    Finished part 2 of js

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -13,7 +13,7 @@
 			var value = document.querySelector('#input_search').value;
 			form.reset();
 
-			getData(value);
+			getData(value.split(' ').join('+'));
 		});
 	};
 
@@ -21,14 +21,19 @@
 	this.getData = function(artist) {
 
 		var http = new XMLHttpRequest();
-		var url = 'https://itunes.apple.com/search?term=audioslave&entity=album';
+		var url = 'https://itunes.apple.com/search?term='+ artist +'&entity=album';
 		var method = 'GET';
+
+		var container = document.querySelector('#album_list_container');
+		container.innerHTML = '';
+
+		console.log(url);
 
 		http.open(method, url);
 		http.onreadystatechange = function() {
 			if(http.readyState === XMLHttpRequest.DONE && http.status === 200) {
 
-				console.log(JSON.parse(http.response));
+				showArtist(JSON.parse(http.response));
 
 			} else if(http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
 				//something failed
@@ -36,6 +41,33 @@
 		}
 
 		http.send();
+
+	};
+
+	this.showArtist = function(obj) {
+		var container = document.querySelector('#album_list_container');
+		var template = '';
+
+		if(obj.results.length > 0) {
+
+			document.querySelector('#not_match').style.display = 'none';
+
+			for(var i = 0; i < obj.results.length; i++) {
+
+				template += '<div class="col-sm-3 album_item">';
+				template +=		'<div class="item_thmb" style="background: url()"></div>';
+				template +=		'<div class="item_title">What the wiw</div>';
+				template +=		'<div class="item_price"><span>Price:</span> $200</div>';
+				template +=		'<a href="#" target="_blank">Buy now</a>';
+				template +=	'</div>';
+			}
+
+			container.innerHTML = '';
+			container.insertAdjacentHTML('afterbegin', template);
+
+		} else {
+			document.querySelector('#not_match').style.display = 'block';
+		}
 
 	};
 
